fabe59514ef3058ae064974ef80f942908915cf4
Tim <tim@brightsidedeveloper.com>
2022-08-11 20:24:48 -0500

    First Style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import tw from 'twin.macro'
 
 function App() {
   return (
-    <div className="App">
-      <H1>Hello World</H1>
-    </div>
+    <FullBackground>
+      <Modal>
+        <Title>Squad Chat</Title>
+        <form>
+          <Input type="email" placeholder='Email' />
+          <Input type="password" placeholder='Password' />
+        </form>
+        <GoogleSignIn>Sign in with Google</GoogleSignIn>
+      </Modal>
+    </FullBackground>
   );
 }
 
-const H1 = tw.h1`text-xl text-center`;
+const FullBackground = tw.div`absolute w-screen h-screen bg-gray-100`
+const Modal = tw.div`m-auto mt-10 bg-white border-2 border-blue-600 rounded-2xl w-[95%] sm:w-[500px] h-[65vh] shadow`
+const Title = tw.h1`text-center text-5xl my-8 text-blue-600`
+const Input = tw.input`block m-auto rounded-md border border-blue-600 mb-8 p-2`;
+const GoogleSignIn = tw.button`block m-auto rounded-md p-4 bg-blue-600 text-white`;
 
 export default App;
