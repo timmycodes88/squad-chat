c841c7a30f6f2c5294699d6b3f1f61b3446245b8
Tim <tim@brightsidedeveloper.com>
2022-08-11 21:27:52 -0500

    Finished Main App

--- src/MainApp.js
+++ src/MainApp.js
@@ -9,19 +9,23 @@ export default function MainApp({user}) {
 
     return(
         <>
-            <PersonBox>
-                <StyledImg src={user.photoURL} />
-                <DisplayName>{user.displayName}</DisplayName>
-            </PersonBox>
-            
+            <Header>
+                <PersonBox>
+                    <StyledImg src={user.photoURL} />
+                    <DisplayName>{user.displayName}</DisplayName>
+                </PersonBox>
+                
+                <Title>Squad Chat</Title>
 
-            <SignOutButton onClick={signOut}>Sign Out</SignOutButton>
+                <SignOutButton onClick={signOut}>Sign Out</SignOutButton>
+            </Header>
         </>
     )
 }
 
-
-const PersonBox = tw.div`m-auto flex items-center mt-4 ml-4`
+const Header = tw.div`flex justify-center border-b h-24 bg-blue-700`;
+const PersonBox = tw.div`flex absolute top-4 left-4 items-center`
+const Title = tw.h1`text-white text-5xl pt-4`
 const StyledImg = tw.img`w-16 h-16 rounded-full mr-4`
-const DisplayName= tw.h2`text-lg`
-const SignOutButton = tw.button`absolute top-4 right-4 bg-green-600 text-white p-4 rounded-md`;
+const DisplayName= tw.h2`text-lg text-white`
+const SignOutButton = tw.button`absolute top-4 right-4 bg-green-600 text-white p-4 rounded-md shadow-md`;
