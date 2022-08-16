292db23d61b509f4a5546aad37c073743caadacf
Tim <tim@brightsidedeveloper.com>
2022-08-15 19:45:58 -0500

    asd

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBeLRcj6dm6-KlldP85kOrmNI1HqXUgqrI",
-  authDomain: "squad-chat-dae54.firebaseapp.com",
-  projectId: "squad-chat-dae54",
-  storageBucket: "squad-chat-dae54.appspot.com",
-  messagingSenderId: "40910615260",
-  appId: "1:40910615260:web:8cc41969096df2401efa19",
-  measurementId: "G-NJ37WT4JYR"
+    authDomain: "squad-chat-dae54.firebaseapp.com",
+    projectId: "squad-chat-dae54",
+    storageBucket: "squad-chat-dae54.appspot.com",
+    messagingSenderId: "40910615260",
+    appId: "1:40910615260:web:8cc41969096df2401efa19",
+    measurementId: "G-NJ37WT4JYR"
 }
   
 export const app = firebase.initializeApp(firebaseConfig);
