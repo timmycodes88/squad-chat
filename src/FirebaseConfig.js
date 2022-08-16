9d34be2b211cbf0bfc11fe1240fc73341b23659d
Tim <tim@brightsidedeveloper.com>
2022-08-15 19:45:49 -0500

    asd

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: "AIzaSyBeLRcj6dm6-KlldP85kOrmNI1HqXUgqrI",
+  authDomain: "squad-chat-dae54.firebaseapp.com",
+  projectId: "squad-chat-dae54",
+  storageBucket: "squad-chat-dae54.appspot.com",
+  messagingSenderId: "40910615260",
+  appId: "1:40910615260:web:8cc41969096df2401efa19",
+  measurementId: "G-NJ37WT4JYR"
 }
   
 export const app = firebase.initializeApp(firebaseConfig);
