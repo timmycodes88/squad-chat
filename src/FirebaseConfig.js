c58739a09963a88447c0df15ccc7a4022c6865fc
Tim <tim@brightsidedeveloper.com>
2022-10-01 14:04:54 -0500

    Updated firebase Config to env

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -11,13 +11,13 @@ onSnapshot
 } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBeLRcj6dm6-KlldP85kOrmNI1HqXUgqrI",
-    authDomain: "squad-chat-dae54.firebaseapp.com",
-    projectId: "squad-chat-dae54",
-    storageBucket: "squad-chat-dae54.appspot.com",
-    messagingSenderId: "40910615260",
-    appId: "1:40910615260:web:8cc41969096df2401efa19",
-    measurementId: "G-NJ37WT4JYR"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 }
   
 const app = firebase.initializeApp(firebaseConfig);
