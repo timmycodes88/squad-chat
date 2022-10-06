f86acbffb999e226f71d755232d43093f3ea8ada
Tim <timvan0118@gmail.com>
2022-10-05 21:26:40 -0500

    Commit

--- src/components/EditableProfile.js
+++ src/components/EditableProfile.js
@@ -1,24 +1,22 @@
 import tw from "twin.macro";
 import { useState } from "react";
-import { updateUserProfile, auth } from "../FirebaseConfig";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { updateUserProfile } from "../FirebaseConfig";
 
-export default function EditableProfile() {
-  const [user] = useAuthState(auth);
+export default function EditableProfile({}) {
   const [newUsername, setNewUsername] = useState("");
 
   return (
-    <Temp>
+    <Wrapper>
       <input
         value={newUsername}
         onChange={(e) => setNewUsername(e.target.value)}
         placeholder="New Username!"
-      ></input>
+      />
       <button onClick={() => updateUserProfile(user.uid, newUsername)}>
         Save
       </button>
-    </Temp>
+    </Wrapper>
   );
 }
 
-const Temp = tw.div`mt-20 mx-auto flex`;
+const Wrapper = tw.div`mt-20 flex justify-center`;
